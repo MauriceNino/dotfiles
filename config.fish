9c745e9bb051a0a3fd98c8167e011ccfc0283042
MauriceNino <mauriceprivat98@gmail.com>
2021-08-06 10:54:12 +0200

    fix exa not starting on newline

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -x fish_greeting ''
 
-bind \ca 'll; commandline -f repaint'
+bind \ca 'printf "\n"; ll; commandline -f repaint'
 bind \cr 'peco_execute_history (commandline -b)' 
 
 zoxide init fish | source
