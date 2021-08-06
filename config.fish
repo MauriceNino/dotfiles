18e44f031e2ce0e57768335cff8e7adfdf1c1ed2
MauriceNino <mauriceprivat98@gmail.com>
2021-08-06 09:47:37 +0200

    update fish peco history function

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set -x fish_greeting ''
 
 bind \ca 'll; commandline -f repaint'
-bind \cr 'peco_put_history (commandline -b)' 
+bind \cr 'peco_execute_history (commandline -b)' 
 
 zoxide init fish | source
 
