d1240998eeb70938991c81f10f749ffbfa20a9a0
MauriceNino <mauriceprivat98@gmail.com>
2026-03-25 22:06:04 +0100

    feat: switch shell; add more default programs for cachy

--- cachyos/setup.sh
+++ cachyos/setup.sh
@@ -13,25 +13,35 @@ yay -S --needed \
   1password \
   atuin \
   bottles \
+  chromium \
+  docker \
+  docker-compose \
   eza \
   fastfetch \
+  flatpak \
+  flatseal \
   firefox \
   ghostty \
   gpu-screen-recorder-gtk \
   gpu-screen-recorder-ui \
+  librewolf \
   mangohud \
   mise \
   moonlight-qt \
   obsidian \
+  steam \
   sunshine \
   vesktop \
+  visual-studio-code-bin \
   zoxide
 
 # Tools setup
 ln -sf ~/dev/dotfiles/common/ghostty/ ~/.config/ghostty
+ln -sf ~/dev/dotfiles/common/MangoHud.conf ~/.config/MangoHud/MangoHud.conf
+ln -sf ~/dev/dotfiles/common/MangoHud.conf ~/.var/app/com.valvesoftware.Steam/config/MangoHud/MangoHud.conf
 
 # Common
-./setup_common_misc.sh
+./../common/setup_misc.sh
 
 # Install mise plugins + default tool versions
 ./../common/setup_mise.sh
--- common/setup_fisher.sh
+++ common/setup_fisher.sh
@@ -2,9 +2,5 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 
 fisher install jorgebucaran/fisher
-fisher install gazorby/fish-exa
-fisher install oh-my-fish/plugin-peco
 fisher install decors/fish-colored-man
-fisher install IlanCosman/tide@v6
-
-tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+fisher install jorgebucaran/hydro
